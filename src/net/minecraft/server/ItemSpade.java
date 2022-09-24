ac1282c9d8719bd5c50c4facb4bbe1e5d35cdfc0
Strultz <69481716+Strultz@users.noreply.github.com>
2022-09-24 06:18:39 -0500

    Fix farmland mining speed

--- src/net/minecraft/server/ItemSpade.java
+++ src/net/minecraft/server/ItemSpade.java
@@ -1,8 +1,10 @@
 package net.minecraft.server;
 
+import pl.moresteck.uberbukkit.Uberbukkit;
+
 public class ItemSpade extends ItemTool {
 
-    private static Block[] bk = new Block[] { Block.GRASS, Block.DIRT, Block.SAND, Block.GRAVEL, Block.SNOW, Block.SNOW_BLOCK, Block.CLAY, Block.SOIL};
+    private static Block[] bk;
 
     public ItemSpade(int i, EnumToolMaterial enumtoolmaterial) {
         super(i, 1, enumtoolmaterial, bk);
@@ -10,5 +12,13 @@ public class ItemSpade extends ItemTool {
 
     public boolean a(Block block) {
         return block == Block.SNOW ? true : block == Block.SNOW_BLOCK;
+    }
+    
+    static {
+        if (Uberbukkit.getPVN() < 12) {
+            bk = new Block[] { Block.GRASS, Block.DIRT, Block.SAND, Block.GRAVEL, Block.SNOW, Block.SNOW_BLOCK, Block.CLAY};
+        } else {
+            bk = new Block[] { Block.GRASS, Block.DIRT, Block.SAND, Block.GRAVEL, Block.SNOW, Block.SNOW_BLOCK, Block.CLAY, Block.SOIL};
+        }
     }
 }
