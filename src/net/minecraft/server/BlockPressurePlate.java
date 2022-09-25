cf0bdc5abd22ddc79ef7e870d50771ded265bfcc
Strultz <69481716+Strultz@users.noreply.github.com>
2022-09-24 20:49:28 -0500

    Update BlockPressurePlate.java

--- src/net/minecraft/server/BlockPressurePlate.java
+++ src/net/minecraft/server/BlockPressurePlate.java
@@ -16,7 +16,7 @@ public class BlockPressurePlate extends Block {
     private EnumMobType a;
 
     protected BlockPressurePlate(int i, int j, EnumMobType enummobtype, Material material) {
-        super(i, j, Uberbukkit.getPVN() >= 12 ? material : Material.ROCK);
+        super(i, j, Uberbukkit.getPVN() >= 12 ? material : Material.STONE);
         this.a = enummobtype;
         this.a(true);
         float f = 0.0625F;
