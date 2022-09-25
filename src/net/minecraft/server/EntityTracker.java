ca92ff8d9d146d6a4cd49f8fa6e1ac77eb6364c0
Strultz <69481716+Strultz@users.noreply.github.com>
2022-09-24 20:38:58 -0500

    Revert tick counts

--- src/net/minecraft/server/EntityTracker.java
+++ src/net/minecraft/server/EntityTracker.java
@@ -39,14 +39,14 @@ public class EntityTracker {
             this.a(entity, 64, 5, true);
         } else if (entity instanceof EntityArrow) {
             // uberbukkit start
-            this.a(entity, 64, 20, Uberbukkit.getPVN() < 12);
+            this.a(entity, 64, Uberbukkit.getPVN() < 12 ? 5 : 20, Uberbukkit.getPVN() < 12);
         } else if (entity instanceof EntitySnowball || (entity instanceof EntityFireball && Uberbukkit.getPVN() < 12)) {
             // uberbukkit end
-            this.a(entity, 64, 10, true);
+            this.a(entity, 64, Uberbukkit.getPVN() < 12 ? 5 : 10, true);
         } else if (entity instanceof EntityFireball && Uberbukkit.getPVN() >= 12) {
             this.a(entity, 64, 10, false);
         } else if (entity instanceof EntityEgg) {
-            this.a(entity, 64, 10, true);
+            this.a(entity, 64, Uberbukkit.getPVN() < 12 ? 5 : 10, true);
         } else if (entity instanceof EntityItem) {
             this.a(entity, 64, 20, true);
         } else if (entity instanceof EntityMinecart) {
