3947f16ebb523e3272c7ff793b6bb2a4195cee27
Strultz <strultzgaming@gmail.com>
2023-03-06 11:39:05 -0600

    Compile!

--- src/net/minecraft/server/EntityPlayer.java
+++ src/net/minecraft/server/EntityPlayer.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.Iterator;
 
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.ChunkCompressionThread;
